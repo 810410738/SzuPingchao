4ba7e73a61042f79c76ef56cb5ea2ed677202c6e
Jerry <810410738@qq.com>
2017-08-31 15:12:06 +0800

    数据库显示提交时间

--- xuanke/admin.py
+++ xuanke/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import UserInfo
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['number']
-admin.site.register(UserInfo,PostAdmin)
+    list_display = ['number', 'time']
+admin.site.register(UserInfo, PostAdmin)
--- xuanke/models.py
+++ xuanke/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class UserInfo(models.Model):
     number = models.CharField(max_length=20)
     passwd = models.CharField(max_length=30)
+    time = models.DateTimeField(auto_now=True)
