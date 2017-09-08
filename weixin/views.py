e1c52eef1051eec34595843da593bca3b7bbb43c
jerry_zj <jerrylzj810410738@gmail.com>
2017-09-08 13:16:53 +0800

    Revert "修改url设置"
    This reverts commit 4063a5de37697c951cb067a3b6a792f748d90fd9.

--- weixin/views.py
+++ weixin/views.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #-*- coding:utf-8 -*-
 from django.http.response import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
@@ -45,3 +46,48 @@ def wechat_main(request):
            except ParseError:
                return HttpResponseBadRequest('Invalid XML Data')
        return HttpResponse(response, content_type="application/xml")
+=======
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, HttpResponseBadRequest
+import hashlib
+import json
+from django.utils.encoding import smart_str
+# Create your views here.
+WEIXIN_TOKEN = 'test'
+
+@csrf_exempt
+def weixin_main(request):
+    """
+       所有的消息都会先进入这个函数进行处理，函数包含两个功能，
+       微信接入验证是GET方法，
+       微信正常的收发消息是用POST方法。
+       """
+    if request.method == 'GET':
+        signature = request.GET.get('signature', None)
+        timestamp = request.GET.get('timestamp', None)
+        nonce = request.GET.get('nonce', None)
+        echostr = request.GET.get('echostr', None)
+        token = WEIXIN_TOKEN
+        if not wechat_instance.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
+            return HttpResponseBadRequest('Verify Failed')
+
+        return HttpResponse(echostr, content_type='text/plain')
+    else:
+        # 解析本次请求的 XML 数据
+        try:
+            wechat_instance.parse_data(data=request.body)
+        except ParseError:
+            return HttpResponseBadRequest('Invalid XML Data')
+         # 获取解析好的微信请求信息
+        message = wechat_instance.get_message()
+
+        if isinstance(message, TextMessage):
+
+
+
+
+
+
+
+>>>>>>> parent of a6ed538... 公众号回复功能（测试）
