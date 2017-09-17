47a99e5e4f344d7adcb670e60a0ae7c499e0bf28
jerry_zj <jerrylzj810410738@gmail.com>
2017-09-17 22:07:05 +0800

    增加报名失败页面

--- szupingxie/views.py
+++ szupingxie/views.py
@@ -22,12 +22,15 @@ def ZhanxinFormPost(request):
             temp = ZhaoXinBaoMing(name=_name,  sex=temp, college=_college, phone=_phone, stu_no=_stu_no)
             temp.save()
             context = {
-                'name':temp.name,
+                'name': temp.name,
                 'id': temp.id
             }
             return render(request, "szupingxie/success.html", context=context)
         else:
-            return HttpResponse("你已经报名了，请不要重复报名，谢谢！")
+            context = {
+                'name': _name,
+            }
+            return render(request, 'szupingxie/fail.html', context=context)
     if request.method == 'GET':
         return render(request, "szupingxie/form.html")
 
